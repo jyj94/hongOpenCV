c7eea65cb02c2fcaed93cab5fb105f93567ffab7
Youngjae Jo <42190339+jyj94@users.noreply.github.com>
2025-09-15 14:58:09 +0900

    09/15

--- Python/OpenCV-PyhtonBook/Chapter07/02.sharpening.py
+++ Python/OpenCV-PyhtonBook/Chapter07/02.sharpening.py
@@ -1,7 +1,18 @@
-import numpy as np, cv2, time
+import cv2, numpy as np
 
-image = cv2.imread("Python/OpenCV-PyhtonBook/Chapter07/images/filter_blur.jpg", cv2.IMREAD_GRAYSCALE)  # 영상 읽기
-if image is None: raise Exception("영상파일 읽기 오류")
+image = cv2.imread('Python/OpenCV-PyhtonBook/Chapter07/images/filter_sharpen.jpg', cv2.IMREAD_GRAYSCALE)
+
+data1 = [
+    0, -1, 0,
+    -1, 5, -1,
+    0, -1, 0
+]
+
+data2 = [
+    -1, -1, -1,
+    -1, 9, -1,
+    -1, -1, -1
+]
 
 def filter(image, mask):
     row, column = image.shape[:2]
@@ -17,13 +28,14 @@ def filter(image, mask):
             dst[i, j] = cv2.sumElems(temp)[0]
     return dst.astype('uint8')
 
-sh
+mask1 = np.array(data1, np.float32).reshape((3, 3))
+mask2 = np.array(data2, np.float32).reshape((3, 3))
 
-mask = [
-    1/9, 1/9, 1/9,
-    1/9, 1/9, 1/9,
-    1/9, 1/9, 1/9
-]
-mask = np.array(mask, np.float32).reshape((3, 3))
-cv2.imshow('result', filter(image, mask=mask))
+sharpen1 = filter(image, mask1)
+sharpen2 = filter(image, mask2)
+
+cv2.imshow("image", image)
+cv2.imshow("sharpen1", sharpen1)
+cv2.imshow("sharpen2", sharpen2)
 cv2.waitKey(0)
+    
